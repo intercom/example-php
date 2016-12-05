f79b44cbf42f1e608ecf1c6094e060e6c7094f8f
Cathal Horan <cathal.horan@intercom.io>
2016-12-05 09:00:04 -0500

    changing id to user_id for first example

--- users/get_user.php
+++ users/get_user.php
@@ -2,8 +2,10 @@
 require "vendor/autoload.php";
 use Intercom\IntercomClient;
 
-$id = $argv[1];
+$usr_id = $argv[1];
 $client = new IntercomClient(getenv('PAT'), null);
-$usr = $client->users->getUser($id);
+$usr = $client->users->getUsers(array(
+  "user_id" => $usr_id
+));
 var_dump($usr);
 ?>
