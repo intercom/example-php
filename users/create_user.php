390818ca4f537a7fe26e03fdcdd31412ad1de911
choran <cathal.horan@intercom.io>
2016-12-22 18:22:19 +0000

    Added extra options for create user

--- users/create_user.php
+++ users/create_user.php
@@ -2,22 +2,41 @@
 require "vendor/autoload.php";
 use Intercom\IntercomClient;
 $longopts  = array(
-    "uid:",     // Required value
+    "uid::",     // Required value
     "email::",    // Optional value
     "uname::",
     "cid::",
     "cname::",
 );
 $options = getopt("", $longopts);
+# Check if either email or user_id has been provided
+if (!array_key_exists('uid', $options) && !array_key_exists('cname', $options)){
+    #Then neither the user_id or email has been provided so exit
+    echo "You need to provide either a user_id or an email \n";
+    return;
+}
+# Initialize the Intercom client
 $intercom = new IntercomClient(getenv('PAT'), null);
+
+# Setup the create structure
+$create_data=(["last_request_at" => time()]);
+
 if (array_key_exists('cid', $options) && array_key_exists('cname', $options)){
     $companies = array("company_id" => $options['cid'],"name" => $options['cname']);
-} 
-$intercom->users->create([
-  "user_id" => $options['uid'],
-  "email" => $options['email'],
-  "name" => $options['uname'],
-  "last_request_at" => time(),
-  "companies" => array($companies)
-]);
+    $create_data["companies"] = array($companies);
+}
+if (array_key_exists('uid', $options)){
+    $create_data["user_id"] = $options['uid'];
+}
+ 
+if (array_key_exists('email', $options)){
+    $create_data["email"] = $options['email'];
+}
+if (array_key_exists('uname', $options)){
+    $create_data["name"] = $options['uname'];
+}
+if (array_key_exists('last_request_at', $options)){
+    $create_data["name"] = $options['last_request_at'];
+}
+$intercom->users->create($create_data);
 ?>
